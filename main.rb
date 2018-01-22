736bb6e188d0415242066e363a9182508eb98dfc
Christophe <christophe.clrd@gmail.com>
2018-01-22 13:43:44 +0100

    Adding next sprint

--- main.rb
+++ main.rb
@@ -1,19 +1,23 @@
 
-def classifier(type, file, *statuses)
-   i = 0
-   tickets = []
+def classifier(file, *statuses)
+   type = ["New Feature", "Improvement", "Task", "Bug"]
 
-  File.open(file).readlines.each do |line|
-    if line.include?(type) && statuses.inject(false) { |memo, status| line.include?(status) || memo }
-      i += 1
-      tickets << line.split("\t")[1]
+  type.each do |type|
+    tickets = []
+     i = 0
+
+    File.open(file).readlines.each do |line|
+      if line.include?(type) && statuses.inject(false) { |memo, status| line.include?(status) || memo }
+        i += 1
+        tickets << line.split("\t")[1]
+      end
     end
-  end
 
-  if i>0
-    puts "#{i} #{type.downcase}#{'s' if i > 1}"
-    puts tickets
-    puts "\n"
+    if i>0
+      puts "#{i} #{type.downcase}#{'s' if i > 1}"
+      puts tickets
+      puts "\n"
+    end
   end
 end
 
@@ -29,13 +33,10 @@ Christophe & Laurent
 
 "
 puts "Tickets delivered during last sprint\n\n"
-classifier('New Feature','last_sprint.txt', 'RELEASED', 'CLOSED', 'DONE')
-classifier('Improvement','last_sprint.txt','RELEASED', 'CLOSED', 'DONE')
-classifier('Task','last_sprint.txt','RELEASED', 'CLOSED', 'DONE')
-classifier('Bug','last_sprint.txt','RELEASED', 'CLOSED', 'DONE')
+classifier('last_sprint.txt', 'RELEASED', 'CLOSED', 'DONE')
 
 puts "Tickets not delivered during last sprint (will be transferred to next sprint)\n\n"
-classifier('New Feature','next_sprint.txt','IN FUNCTIONAL REVIEW','IN REVIEW','IN DEVELOPMENT','OPEN / READY FOR DEV')
-classifier('Improvement','next_sprint.txt','IN FUNCTIONAL REVIEW','IN REVIEW','IN DEVELOPMENT','OPEN / READY FOR DEV')
-classifier('Task','next_sprint.txt','IN FUNCTIONAL REVIEW','IN REVIEW','IN DEVELOPMENT','OPEN / READY FOR DEV')
-classifier('Bug','next_sprint.txt','IN FUNCTIONAL REVIEW','IN REVIEW','IN DEVELOPMENT','OPEN / READY FOR DEV')
+classifier('last_sprint.txt','IN FUNCTIONAL REVIEW','IN REVIEW','IN DEVELOPMENT','OPEN / READY FOR DEV')
+
+puts "Other tickets planned for next sprint)\n\n"
+classifier('next_sprint.txt','IN FUNCTIONAL REVIEW','IN REVIEW','IN DEVELOPMENT','OPEN / READY FOR DEV')
